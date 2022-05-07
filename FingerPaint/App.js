9d4c2b277c4974e1491fec1c3ee41c3b084995d9
Nicholas Diaz <nicholasdiazjr509@gmail.com>
2022-05-07 09:53:40 -0500

    importing canvas

--- FingerPaint/App.js
+++ FingerPaint/App.js
@@ -1,19 +1,29 @@
-import React,{useState} from 'react';
-import {View, Switch, Text, Stylesheet} from 'react-native';
-import styles from './styles'
+import React, {useLayoutEffect, useState} from 'react';
+// import {View, Switch, Text, Stylesheet} from 'react-native';
+import rough from 'roughjs/bundled/rough.esm';
+// import styles from './styles';
 
-export default function App(){
-  const [location,setLocation] = useState(false)
-  const [mic,setMic] = useState(false)
-  const [storage,setStorage] = useState(false)
- 
+const generator = rough.generator();
+
+const App = () => {
+  const [elements, setElements] = useState([]);
+
+  useLayoutEffect(() => {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    const roughCanvas = rough.canvas(canvas);
+    const rect = generator.rectangle(10, 10, 100, 100);
+    const line = generator.line(10, 10, 110, 100);
+    roughCanvas.draw(rect);
+    roughCanvas.draw(line);
+  });
 
   return (
-    <View style={styles.container}>
-      <Text style={{color: 'darkslateblue', fontSize: 30}}>Hello World</Text>
-    </View>
+    <canvas id="canvas" width={window.innerWidth} height={window.innerHeight} >
+      Canvas
+    </canvas>
   );
-  
-  }
+};
 
- 
+export default App;
