e0b3e7f044250333c7265d7c23750fef084d542f
Nicholas Diaz <nicholasdiazjr509@gmail.com>
2022-05-14 08:32:01 -0500

    Now I know what was wrong.

--- fingerpaint-app/src/App.js
+++ fingerpaint-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
+  const canvasRef = useRef(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas
+        style={{
+          border: "1px solid #000",
+        }}
+        width={400}
+        height={400}
+        ref={canvasRef}
+      />
     </div>
   );
 }
