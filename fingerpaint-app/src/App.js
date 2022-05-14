d40dd1442f268babba8d43d937fb7da4237fe659
Nicholas Diaz <nicholasdiazjr509@gmail.com>
2022-05-14 10:15:00 -0500

    adding colors in a dropdown

--- fingerpaint-app/src/App.js
+++ fingerpaint-app/src/App.js
@@ -2,6 +2,8 @@ import React, { useRef } from "react";
 // import logo from './logo.svg';
 import "./App.css";
 
+const colors = ["red", "blue", "green", "black", "yellow"];
+
 function App() {
   const canvasRef = useRef(null);
 
@@ -11,10 +13,19 @@ function App() {
         style={{
           border: "1px solid #000",
         }}
-        width={400}
-        height={400}
+        width={600}
+        height={600}
         ref={canvasRef}
       />
+      <br />
+      <select>
+        {colors.map((color) => (
+          <option value={color}>{color}</option>
+        ))}
+      </select>
+      <button>Clear</button>
+      <br />
+      <button>Download</button>
     </div>
   );
 }
